77e3f30a33b35d26c86eb497cab6bbd9e2954d52
Guo Weixuan <cnmozzie@gmail.com>
2022-03-27 21:24:27 +0800

    <fix>: combine the process of transfer BNB and NFT

--- contracts/NFTCollection/NFTMarketplace.sol
+++ contracts/NFTCollection/NFTMarketplace.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import './NFTCollection.sol';
@@ -7,10 +8,13 @@ contract NFTMarketplace {
   NFTCollection nftCollection;
   IERC20 lowbToken;
 
-  mapping (uint256 => uint256) private listToken;
+  mapping (uint256 => uint256) private tokenPrice;
+  mapping (uint256 => address) private tokenSeller;
 
   event saleNFT(uint indexed tokenId, uint256 price);
   event buyNFT(address from, address to, uint256 tokenId, uint256 price);
+
+  receive() external payable {}
 
 
   constructor (address _address, address _lowbAddress) {
@@ -20,15 +24,17 @@ contract NFTMarketplace {
 
   function prepareForSale(uint256 tokenId, uint256 price) public {
     require(msg.sender != address(0));
-    listToken[tokenId] = price;
+    tokenPrice[tokenId] = price;
+    tokenSeller[tokenId] = msg.sender;
     nftCollection.transferFrom(msg.sender, address(this), tokenId);
     emit saleNFT(tokenId, price);
   }
 
-  function sellNFT(address from, uint256 tokenId, uint256 _price) public {
-    uint256 price = listToken[tokenId];
-    require(price == _price, "The price is incorrect");
+  function sellNFT(address from, uint256 tokenId) payable public {
+    uint256 price = tokenPrice[tokenId];
+    require(msg.value >= price, "INSUFFICIENT BNB"); 
     nftCollection.transferFrom(address(this), msg.sender, tokenId);
+    payable(msg.sender).transfer(price);
     emit buyNFT(from, msg.sender, tokenId, price);
   }
 }
